a23d0f7734954724620983f1f5c857245f501e7e
muhammadzamanshah <zamanluckybangash@gmail.com>
2022-05-26 17:42:44 +0500

    remove all commnets

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: FutureBuilder(
@@ -35,12 +27,12 @@ class MyApp extends StatelessWidget {
         builder: (context , snapshot){
           if(snapshot.hasError){
             print('you have an error! ${snapshot.error.toString()}');
-            return Text("Something went wrong");
+            return const Text("Something went wrong");
           }else{
             if(snapshot.hasData){
-              return MyHomePage(title: 'Flutter Demo Hom');
+              return const MyHomePage(title: 'Flutter Demo Hom');
             }else{
-              return Center(
+              return const Center(
               child: CircularProgressIndicator());
           }
           }
@@ -52,15 +44,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -84,13 +67,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -108,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
